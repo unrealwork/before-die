d0f7d6fb2e7bc3885bc6c89d57bae718cd9c1a2c
unrealwork <unrealwork@gmail.com>
2018-02-17 14:07:07 +0300

    added slf4j

--- api-service/src/main/java/com/unrealwork/beforedie/api/GreetingController.java
+++ api-service/src/main/java/com/unrealwork/beforedie/api/GreetingController.java
@@ -1,21 +1,21 @@
 package com.unrealwork.beforedie.api;
 
 import com.unrealwork.beforedie.models.Greeting;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class GreetingController {
   private static final String TEMPLATE = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
-  @RequestMapping(value = "/api", method = RequestMethod.GET)
-  @ResponseBody
+  @GetMapping("greeting")
   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
     return new Greeting(counter.incrementAndGet(),
         String.format(TEMPLATE, name));
--- api-service/src/main/java/com/unrealwork/beforedie/config/ServiceConfiguration.java
+++ api-service/src/main/java/com/unrealwork/beforedie/config/ServiceConfiguration.java
@@ -3,10 +3,13 @@ package com.unrealwork.beforedie.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("com.unrealwork.beforedie")
+@ComponentScan(basePackages = "com.unrealwork.beforedie")
+@PropertySource("classpath:application.properties")
 public class ServiceConfiguration {
 }
