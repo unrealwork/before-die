f969b4ebbca2368ef34a06e376100f59e8fcbcda
unrealwork <unrealwork@gmail.com>
2018-02-17 14:42:09 +0300

    added localization

--- api-service/src/main/java/com/unrealwork/beforedie/api/GreetingController.java
+++ api-service/src/main/java/com/unrealwork/beforedie/api/GreetingController.java
@@ -1,23 +1,34 @@
 package com.unrealwork.beforedie.api;
 
 import com.unrealwork.beforedie.models.Greeting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import sun.rmi.runtime.Log;
 
+import java.util.Locale;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/api")
 public class GreetingController {
+  private Logger logger = LoggerFactory.getLogger(GreetingController.class);
   private static final String TEMPLATE = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
+  @Autowired
+  private MessageSource messageSource;
+
   @GetMapping("greeting")
-  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+  public Greeting greeting(Locale locale, @RequestParam(value = "name", defaultValue = "World") String name) {
+    logger.debug("The language is {}", locale.getDisplayLanguage());
+    final String greetingMessage = messageSource.getMessage("greeting", new String[] {name}, new Locale("ru"));
     return new Greeting(counter.incrementAndGet(),
-        String.format(TEMPLATE, name));
+        greetingMessage);
   }
 }
